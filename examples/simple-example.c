c9084b0721abd541dce9dd42578bf2c081f9a7ff
David Edmundson <kde@davidedmundson.co.uk>
2023-02-16 11:15:25 +0000

    update example

--- examples/simple-example.c
+++ examples/simple-example.c
@@ -9,7 +9,7 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include "gtk-layer-shell.h"
+#include "gtk-pip-shell.h"
 #include <gtk/gtk.h>
 
 static void
@@ -20,31 +20,8 @@ activate (GtkApplication* app, void *_data)
     // Create a normal GTK window however you like
     GtkWindow *gtk_window = GTK_WINDOW (gtk_application_window_new (app));
 
-    // Before the window is first realized, set it up to be a layer surface
-    gtk_layer_init_for_window (gtk_window);
-
-    // Order below normal windows
-    gtk_layer_set_layer (gtk_window, GTK_LAYER_SHELL_LAYER_BOTTOM);
-
-    // Push other windows out of the way
-    gtk_layer_auto_exclusive_zone_enable (gtk_window);
-
-    // We don't need to get keyboard input
-    // gtk_layer_set_keyboard_mode (gtk_window, GTK_LAYER_SHELL_KEYBOARD_MODE_NONE); // NONE is default
-
-    // The margins are the gaps around the window's edges
-    // Margins and anchors can be set like this...
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_LEFT, 40);
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_RIGHT, 40);
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_TOP, 20);
-    // gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_BOTTOM, 0); // 0 is default
-
-    // ... or like this
-    // Anchors are if the window is pinned to each edge of the output
-    static const gboolean anchors[] = {TRUE, TRUE, FALSE, TRUE};
-    for (int i = 0; i < GTK_LAYER_SHELL_EDGE_ENTRY_NUMBER; i++) {
-        gtk_layer_set_anchor (gtk_window, i, anchors[i]);
-    }
+    // Before the window is first realized, set it up to be a pip surface
+    gtk_pip_init_for_window (gtk_window);
 
     // Set up a widget
     GtkWidget *label = gtk_label_new ("");
